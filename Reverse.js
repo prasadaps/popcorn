12f0c260e31f4449eb6d384f95c5037fc3dcfa3e
Prasad Phate (Tata Consultancy Services Ltd) <v-prphat@microsoft.com>
2019-07-09 12:05:23 +0530

    Initial Commit

---
+++ Reverse.js
@@ -0,0 +1,8 @@
+//reverse function 
+function reverseString(str) {
+    str = str.split(''); //string to array
+    str = str.reverse(); //reverse the order 
+    str = str.join(''); //then join the reverse order values
+    return str;
+}
+reverseString("prasad");
