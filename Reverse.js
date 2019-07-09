3615b2c9f9edbac2fd62b2ea46acc353a98b247d
Prasad Phate (Tata Consultancy Services Ltd) <v-prphat@microsoft.com>
2019-07-09 13:37:56 +0530

    Initial Commit

---
+++ Reverse.js
@@ -0,0 +1,13 @@
+//reverse function 
+function reverseString(str1) {
+    str1 = str1.split(''); //string to array
+    str1 = str1.reverse(); //reverse the order 
+    str1 = str1.join(''); //then join the reverse order values
+    return str;
+}
+
+flksdjf 
+console.log("I'M adding the new code to this project");
+ffdsfj
+reverseString("PS2");
+    l[package;KeyboardEvent]
